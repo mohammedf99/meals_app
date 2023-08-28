eba372a1d8c10982a5b54a9c3c74130dc85f3587
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-08-28 14:36:53 +0300

    added navigation function

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+import '../data/dummy_data.dart';
+import './meals_screen.dart';
+
+import '../models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    // alternative: Navigator.of(context).push(route)
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,19 +30,19 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pick your category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(16),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text("1", style: TextStyle(color: Colors.white),),
-          Text("1", style: TextStyle(color: Colors.white),),
-          Text("1", style: TextStyle(color: Colors.white),),
-          Text("1", style: TextStyle(color: Colors.white),),
-          Text("1", style: TextStyle(color: Colors.white),),
-          Text("1", style: TextStyle(color: Colors.white),),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectedTab: _selectCategory,
+            ),
         ],
       ),
     );
