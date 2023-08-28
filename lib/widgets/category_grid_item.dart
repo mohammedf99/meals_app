982d32749705da28aa492f873b04d9b90abc2686
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-08-28 14:37:19 +0300

    styled category grid item

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
+import '../models/category.dart';
+
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key});
+  const CategoryGridItem({super.key, required this.category, required this.onSelectedTab});
+
+  final Category category;
+  final void Function(BuildContext context, Category category) onSelectedTab;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return InkWell(
+      onTap: () => onSelectedTab(context, category),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.7),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+      ),
+    );
   }
 }
