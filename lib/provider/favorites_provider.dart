433f0e06675efe042be7d7f53d18ab8e1de185ef
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-09-01 13:54:27 +0300

    added providers

---
+++ lib/provider/favorites_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    // state is a globally accessible variable provided by StateNotifier.
+    final isMealFavorite = state.contains(meal);
+
+    if (isMealFavorite) {
+      //where is used because it returns a new list. and that's how stateNotifier works.
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>(
+        (ref) => FavoriteMealsNotifier());
