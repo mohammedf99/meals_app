e3ba91254e040a19e99662bb20f19fbcb0a65ba3
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-09-02 12:10:27 +0300

    added boolean to the method to manage UI

--- lib/provider/favorites_provider.dart
+++ lib/provider/favorites_provider.dart
@@ -5,15 +5,17 @@ import '../models/meal.dart';
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
 
-  void toggleMealFavoriteStatus(Meal meal) {
+  bool toggleMealFavoriteStatus(Meal meal) {
     // state is a globally accessible variable provided by StateNotifier.
     final isMealFavorite = state.contains(meal);
 
     if (isMealFavorite) {
       //where is used because it returns a new list. and that's how stateNotifier works.
       state = state.where((m) => m.id != meal.id).toList();
+      return false;
     } else {
       state = [...state, meal];
+      return true;
     }
   }
 }
