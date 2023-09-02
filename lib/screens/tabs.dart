c723f8c6a8d1b4e70fd7a2cb5d8361eb5b553ca3
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-09-02 12:34:07 +0300

    managing the data through filteredMealProvider

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -6,7 +6,6 @@ import './meals_screen.dart';
 import 'filters_screen.dart';
 import '../widgets/main_drawer.dart';
 
-import '../provider/meals_provider.dart';
 import '../provider/favorites_provider.dart';
 import '../provider/filters_provider.dart';
 
@@ -47,23 +46,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegeterian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(filteredMealProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
